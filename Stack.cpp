789e682879eb121ac17271b3f8c5e51000b817c9
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-22 10:07:37 +0700

    membuat node top untuk pointer sementara

--- Stack.cpp
+++ Stack.cpp
@@ -37,6 +37,12 @@ class Stack{
         if (isEmpty()){
             cout << "Stack is empty." << endl;
         }
+
+        Node* temp = top; // Create a temporary pointer to the top node
+        top = top->next; // Update the top pointer to the next node
+        cout << "Popped value: " << top->data << endl;
+
+        
     }
 };
 
