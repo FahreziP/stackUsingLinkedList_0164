05db8b519fbd8df28c3bb9e4f8a641281e76fea9
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-22 10:17:39 +0700

    membuat fungsi if else di dalam void peek

--- Stack.cpp
+++ Stack.cpp
@@ -43,9 +43,19 @@ class Stack{
         cout << "Popped value: " << top->data << endl;
     }
 
-    // Peek/Top operation: Retrieve the value of the topmost element without removing
+    // Peek/Top operation: Retrieve the value of the topmost element without removing it
     void peek(){
-        
+        if (top == NULL){
+            cout << "List is empty." << endl;
+        }
+        else{
+            Node *current = top;
+            while (current != NULL){
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }// Return the Value of the top node
     }
 };
 
