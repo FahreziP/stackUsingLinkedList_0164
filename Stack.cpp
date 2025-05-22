16048de7160c2c971a25679d7cc46629fb78a205
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-22 10:11:23 +0700

    membuat fungsi void peek

--- Stack.cpp
+++ Stack.cpp
@@ -41,7 +41,10 @@ class Stack{
         Node* temp = top; // Create a temporary pointer to the top node
         top = top->next; // Update the top pointer to the next node
         cout << "Popped value: " << top->data << endl;
+    }
 
+    // Peek/Top operation: Retrieve the value of the topmost element without removing
+    void peek(){
         
     }
 };
