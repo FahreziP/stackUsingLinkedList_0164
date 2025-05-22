b62313d7e3fc4818a7e53bf9536142e74130dd0c
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-22 09:44:37 +0700

    menambahkan class Node

--- Stack.cpp
+++ Stack.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+//Node class representing a single node in the lindked list
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(){
+        next = NULL;
+    }
+};
+
 int main (){
     
 }
