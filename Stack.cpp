b2640a91a47177857b868a808bdbf7f5c0a05a54
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-22 09:50:57 +0700

    membuat class stack

--- Stack.cpp
+++ Stack.cpp
@@ -3,13 +3,25 @@ using namespace std;
 
 //Node class representing a single node in the lindked list
 class Node{
-public:
-    int data;
-    Node* next;
+    public:
+        int data;
+        Node* next;
 
     Node(){
         next = NULL;
     }
+};
+
+// Stack class
+class Stack{
+    private:
+        Node* top; // Pointer to the top node of the stack
+
+    public:
+        Stack(){
+            top = NULL; // Initialize the stack with a null top pointer
+        }
+
 };
 
 int main (){
